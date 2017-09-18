dfb59e5b2ee25ebf832bb13e24b9699cc4e1eee6
Jessica Greene <jessica0greene@gmail.com>
2017-09-18 18:39:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,8 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key];
   return newObj
 }
+
+ destructivelyDeleteFromObjectByKey(object, key){
+   delete object[key];
+   return object
+ }
